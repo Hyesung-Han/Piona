4ece73263b2bf0ee6596f83e22b02276c8662f02
오윤택 <xoem00@gmail.com>
2022-04-21 15:59:20 +0900

    :wrench: Add first setting

---
+++ web/backend/bloom/src/main/java/com/jeans/bloom/BloomApplication.java
@@ -0,0 +1,13 @@
+package com.jeans.bloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomApplication.class, args);
+    }
+
+}
