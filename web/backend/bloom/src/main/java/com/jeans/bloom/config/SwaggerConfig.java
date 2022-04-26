6ee0f5bdf377b19b35a78cba0312afab0b9b2988
Hyesung-Han <hanhs4544@gmail.com>
2022-04-26 11:03:13 +0900

    🔧 web/be init

--- web/backend/bloom/src/main/java/com/jeans/bloom/config/SwaggerConfig.java
+++ web/backend/bloom/src/main/java/com/jeans/bloom/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig{
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.jeans.bloom.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.jeans.bloom.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
