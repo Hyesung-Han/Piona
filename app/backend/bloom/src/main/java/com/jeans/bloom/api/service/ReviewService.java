e8da268994a27bbd268888c0b370039a83bf2d8c
wjddk7507 <wjddk9494@gmail.com>
2022-04-28 17:21:53 +0900

    🔧 Add write review API

--- app/backend/bloom/src/main/java/com/jeans/bloom/api/service/ReviewService.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/api/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.jeans.bloom.api.service;
 
+import com.jeans.bloom.api.request.ReviewWriteReq;
 import com.jeans.bloom.api.response.ReviewDetailRes;
 import com.jeans.bloom.db.entity.type.StatusType;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface ReviewService {
 
     List<ReviewDetailRes> findReviewsByReservation_Shop_ShopNumberAndIsBan(String shopNumber, StatusType isBan) throws Exception;
+
+    void writeReview(ReviewWriteReq reviewWriteReq) throws Exception;
 }
