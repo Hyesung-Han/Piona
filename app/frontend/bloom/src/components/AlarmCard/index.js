58818a02a890810cdc1b375cb721ada5837b15db
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-04-28 16:14:40 +0900

    🔧 Add AlarmCard

---
+++ app/frontend/bloom/src/components/AlarmCard/index.js
@@ -0,0 +1,74 @@
+import React, {useState, useCallback} from 'react';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
+import HorizonLine from '../../utils/HorizonLine';
+import Honeybee from '../../assets/Honeybee.png';
+
+/**
+ * CSW | 2022.04.28
+ * @name AlarmCard
+ * @api x
+ * @des
+ * 1. 컴포넌트 목록 :
+ * 2. 페이지 기능 :
+ * FlatList에 보여줄 item 컴포넌트이다.
+ */
+
+const AlarmCard = ({item}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.CardList}>
+        <View style={styles.AlarmContent}>
+          <Text style={styles.content}> {item.content} </Text>
+          <Text style={styles.createdAt}> {item.content} </Text>
+        </View>
+        <View style={styles.iconBox}>
+          <Image
+            source={Honeybee}
+            style={{
+              width: 40,
+              height: 40,
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  CardList: {
+    width: '90%',
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    elevation: 5,
+    height: 70,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  AlarmContent: {
+    flex: 2,
+    marginLeft: 20,
+  },
+  iconBox: {
+    position: 'relative',
+    right: 30,
+    top: 13,
+  },
+  content: {
+    fontSize: 15,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  createdAt: {
+    fontSize: 10,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+});
+
+export default AlarmCard;
--- app/frontend/bloom/src/pages/Alarm/index.js
+++ app/frontend/bloom/src/pages/Alarm/index.js
@@ -1,18 +1,108 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {useFocusEffect} from '@react-navigation/native';
-import {View, Dimensions, Alert, Text} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import AlarmCard from '../../components/AlarmCard';
+
+/**
+ * CSW | 2022.04.28
+ * @name AlarmPage
+ * @des
+ * 알람 보여주는 페이지입니다.
+ *  */
 
 const AlarmPage = ({navigation}) => {
+  const DATA = [
+    //괄호 하나하나가 item이 된다.
+    {
+      content: '호진이가게',
+    },
+    {
+      content: '소원이가게',
+    },
+    {
+      content: '혜성이가게',
+    },
+    {
+      content: '동준이형가게',
+    },
+    {
+      content: '정아누나가게',
+    },
+    {
+      content: '윤택이형가게',
+    },
+    {
+      content: '호진이가게',
+    },
+  ];
+
+  const renderItem = ({item}) => {
+    return <AlarmCard item={item} />;
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#F9F9F9',
-      }}>
-      <Text>AlarmPage</Text>
+    <View style={styles.container}>
+      <FlatList
+        //리스트의 소스를 담는 속성
+        //data={data}
+        data={DATA}
+        //data로 받은 소스의 아이템들을 render 시켜주는 콜백함수
+        renderItem={renderItem}
+        //item의 고유의 키를 부여하는 속성
+        keyExtractor={item => String(item.id)}
+        //무한 스크롤때문에 넣은듯
+        // onEndReached={() => {if(loading===false && pageNum<=totalPageCnt) getMyPillHistoryList()}}
+        // onEndReachedThreshold={0.4}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F8F8',
+  },
+  textInput: {
+    fontSize: 14,
+    marginLeft: 10,
+  },
+  inputBox: {
+    flex: 1,
+    borderStyle: 'solid',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    width: '70%',
+    height: '11%',
+    borderRadius: 10,
+    borderColor: '#F2A7B3',
+    borderWidth: 1.5,
+    marginTop: '9%',
+    marginBottom: '9%',
+  },
+  iconBox: {
+    flex: 1,
+    position: 'absolute',
+    width: '20%',
+    top: 5,
+    right: 0,
+  },
+  searchBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  list: {
+    flex: 3,
+  },
+});
+
 export default AlarmPage;
