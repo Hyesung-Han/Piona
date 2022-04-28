b03aba56f80b56496d33f003ef4d6a17326ea088
hanhs4544@naver.com <Gptjd1220!>
2022-04-28 12:25:19 +0900

    🔧 Add ItemList

--- app/backend/bloom/src/main/java/com/jeans/bloom/api/controller/ShopController.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/api/controller/ShopController.java
@@ -1,14 +1,17 @@
 package com.jeans.bloom.api.controller;
 
-import com.jeans.bloom.common.response.BaseResponseBody;
+import com.jeans.bloom.api.response.ItemRes;
 import com.jeans.bloom.api.response.ShopRes;
 import com.jeans.bloom.api.service.ShopService;
+import com.jeans.bloom.common.response.BaseResponseBody;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(value = "가게 API", tags = {"Shop"})
 @RequestMapping("/api/shop")
@@ -35,6 +38,18 @@ public class ShopController {
                 return ResponseEntity.status(403).body(BaseResponseBody.of( "success", "해당 가게가 없습니다."));
             else
                 return ResponseEntity.status(200).body(BaseResponseBody.of("success",shopRes));
+        }catch  (Exception e){
+            return ResponseEntity.status(200).body(BaseResponseBody.of("fail",e));
+        }
+
+    }
+    @GetMapping("/item")
+    @ApiOperation(value = "상품 리스트 조회", notes = "shop number로 해당 가게 상품리스트를 조회한다.")
+    public ResponseEntity<BaseResponseBody> findItemsByShop_ShopNumber(
+            @RequestParam @ApiParam(value = "조회할 shop_number", required = true) String shopNumber){
+        try{
+            List<ItemRes> itemList = shopService.findItemsByShop_ShopNumber(shopNumber);
+            return ResponseEntity.status(200).body(BaseResponseBody.of("success",itemList));
         }catch  (Exception e){
             return ResponseEntity.status(200).body(BaseResponseBody.of("fail",e));
         }
