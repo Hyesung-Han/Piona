5cc56d93e9ac5b415f9a757de3944770face4b7f
wjddk7507 <wjddk9494@gmail.com>
2022-04-28 11:24:12 +0900

    🔧 Add add cart item API

--- app/backend/bloom/src/main/java/com/jeans/bloom/api/controller/CartController.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/api/controller/CartController.java
@@ -1,17 +1,15 @@
 package com.jeans.bloom.api.controller;
 
-import com.jeans.bloom.common.response.BaseResponseBody;
+import com.jeans.bloom.api.request.CartReq;
 import com.jeans.bloom.api.response.CartRes;
 import com.jeans.bloom.api.service.CartService;
+import com.jeans.bloom.common.response.BaseResponseBody;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,8 +39,26 @@ public class CartController {
         try{
             List<CartRes> cartList = cartService.findCartsByUser_UserId(userId);
             return ResponseEntity.status(200).body(BaseResponseBody.of( "success", cartList));
-        } catch (Error e) {
-            return ResponseEntity.status(403).body(BaseResponseBody.of( "fail", "장바구니 리스트를 조회할 수 없습니다"));
+        } catch (Exception e) {
+            return ResponseEntity.status(403).body(BaseResponseBody.of( "fail", e));
+        }
+    }
+
+    /**
+     * LJA | 2022.04.28
+     * @name addCartItem
+     * @api {post} /cart
+     * @des 아이템 정보를 받아 장바구니(cart)에 아이템을 추가해주는 메소드
+     */
+    @PostMapping
+    @ApiOperation(value = "장바구니에 아이템 추가", notes = "아이템 정보를 입력받아 cart에 추가한다")
+    public ResponseEntity<BaseResponseBody> addCartItem(
+            @RequestBody @ApiParam(value="아이디", required = true) CartReq cartItem) {
+        try{
+            cartService.addCartItem(cartItem);
+            return ResponseEntity.status(200).body(BaseResponseBody.of( "success"));
+        } catch (Exception e) {
+            return ResponseEntity.status(403).body(BaseResponseBody.of( "fail", e));
         }
     }
 
--- app/backend/bloom/src/main/java/com/jeans/bloom/api/service/CartServiceImpl.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/api/service/CartServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jeans.bloom.api.service;
 
+import com.jeans.bloom.api.request.CartReq;
 import com.jeans.bloom.api.response.CartRes;
 import com.jeans.bloom.db.entity.Cart;
 import com.jeans.bloom.db.repository.CartRepository;
@@ -27,8 +28,18 @@ public class CartServiceImpl implements CartService{
      * @des 회원 아이디를 입력받아 회원의 장바구니 리스트를 리턴해주는 메소드
      */
     @Override
-    public List<CartRes> findCartsByUser_UserId(String userId) {
+    public List<CartRes> findCartsByUser_UserId(String userId) throws Exception {
         Optional<List<Cart>> optionalCarts = cartRepository.findCartsByUser_UserId(userId);
         return optionalCarts.map(carts -> carts.stream().map(cart -> CartRes.of(cart)).collect(Collectors.toList())).orElse(null);
+    }
+
+    /**
+     * LJA | 2022.04.28
+     * @name addCartItem
+     * @des 아이템 정보를 받아 장바구니(cart)에 아이템을 추가해주는 메소드
+     */
+    @Override
+    public void addCartItem(CartReq cartItem) throws Exception {
+        cartRepository.save(cartItem.toCart());
     }
 }
