d17d0afa5959c9434cca585520eb176f06660726
오윤택 <xoem00@gmail.com>
2022-04-28 15:17:38 +0900

    🔧 Add 인증번호 발송 api

--- app/backend/bloom/src/main/java/com/jeans/bloom/config/SecurityConfig.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/config/SecurityConfig.java
@@ -76,7 +76,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
-                .antMatchers("/api/user/signin", "/api/user/signup", "/api/user/idcheck", "/api/user/nickCheck").permitAll()     //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+                .antMatchers("/api/user/signin",
+                        "/api/user/signup",
+                        "/api/user/idcheck",
+                        "/api/user/nickCheck",
+                        "/api/user/phoneRequest").permitAll()     //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
                 .anyRequest().authenticated()
                 .and().cors();
     }
