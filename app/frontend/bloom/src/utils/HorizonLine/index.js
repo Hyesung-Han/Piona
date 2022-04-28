f46b1fd69db98f63d8a052a8f05ec89d3340a2c1
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-04-28 14:03:01 +0900

    🔧 Add HorizonLine

---
+++ app/frontend/bloom/src/utils/HorizonLine/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const HorizonLine = () => {
+  return (
+    <View
+      style={{
+        width: '95%',
+        borderBottomColor: '#aaa',
+        borderBottomWidth: 0.3,
+        lineHeight: '0.1em',
+        marginBottom: '10%',
+      }}></View>
+  );
+};
+
+export default HorizonLine;
