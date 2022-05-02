e9e6a9b9d8ffc706483c4ef9900f630b5eff0d81
오윤택 <xoem00@gmail.com>
2022-05-02 13:50:23 +0900

    🔧 Add Web Setting

---
+++ web/backend/bloom/src/main/java/com/jeans/bloom/db/entity/ReviewComment.java
@@ -0,0 +1,29 @@
+package com.jeans.bloom.db.entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+@Table(name = "review_comment_t", schema = "bloom")
+public class ReviewComment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Integer commentId;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(name = "content", length = 255)
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
