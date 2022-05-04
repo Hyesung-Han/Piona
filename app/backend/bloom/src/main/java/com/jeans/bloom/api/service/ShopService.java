05de280e54b9cb30f44556d667f92fffcf3db28c
wjddk7507 <wjddk9494@gmail.com>
2022-05-04 16:30:19 +0900

    🔧 Add unable reservation date return API

--- app/backend/bloom/src/main/java/com/jeans/bloom/api/service/ShopService.java
+++ app/backend/bloom/src/main/java/com/jeans/bloom/api/service/ShopService.java
@@ -3,10 +3,13 @@ package com.jeans.bloom.api.service;
 import com.jeans.bloom.api.response.ItemRes;
 import com.jeans.bloom.api.response.ShopRes;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ShopService {
     ShopRes findShopByShopNumber(String shopNumber) throws Exception;
 
     List<ItemRes> findItemsByShop_ShopNumber(String shopNumber) throws Exception;
+
+    List<Date> getUnableDate(int itemId, int quantity) throws Exception;
 }
