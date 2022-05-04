9db07d1d4e7ee4ed4fe3a9cf1111c091e8bfbde7
LDJ <ehdwnsdl1210@gmail.com>
2022-05-04 11:22:47 +0900

    🔨 Add Redux Settings

--- app/frontend/bloom/src/redux/slices/user.js
+++ app/frontend/bloom/src/redux/slices/user.js
@@ -1,5 +1,12 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+// store -> root reducer(state) -> user slice
+// state.user.id
+
+// action: state를 바꾸는 행위/동작
+// dispatch: 그 액션을 실제로 실행하는 함수
+// reducers: 액션이 실제로 실행되면 state를 바꾸는 로직
+
 const initialState = {
   name: '',
   id: '',
