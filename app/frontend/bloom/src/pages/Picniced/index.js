bd4e1d66799ac601717ca8a515bae16f1906e55e
이호진 <roycmlhj@gmail.com>
2022-05-04 17:25:52 +0900

    🔧 Add Picnic Page API

--- app/frontend/bloom/src/pages/Picniced/index.js
+++ app/frontend/bloom/src/pages/Picniced/index.js
@@ -3,11 +3,13 @@ import {useFocusEffect} from '@react-navigation/native';
 import {View, Dimensions, Alert, Text, FlatList} from 'react-native';
 import DoneCard from '../../components/DoneCard';
 import RegisterReview from '../RegisterReview';
+import {getMyReservationList} from '../../utils/Axios';
+import {useSelector} from 'react-redux';
 
 /**
- * LHJ | 2022.05.02
+ * LHJ | 2022.05.04
  * @name PicnicedPage
- * @api /picnic, /review
+ * @api getMyReservationList
  * @des
  * 1. 컴포넌트 목록 : DoneCard,FlatList
  * 2. 페이지 기능 :
@@ -22,92 +24,32 @@ const PicnicedPage = ({navigation}) => {
   const [data, setData] = useState([]);
   const [offset, setOffset] = useState(0);
   const [loading, setLoading] = useState(false);
+  //받아오고 그냥 바로 쓰면 됨
+  const user_id = useSelector(state => state.user.id);
 
-  //getData에 완료된 예약 정보 가져오는 api를 넣자!!!
-  const getData = () => {
-    setLoading(true);
-    fetch('http://jsonplaceholder.typicode.com/posts')
-      //해당 api를 통해서 받아오는 정보는 userId, id, title, body이다.
-      .then(res => res.json())
-      .then(res => setData(res));
+  //picnicing에서 사용했던 api를 그대로 사용하여 프론트단에서 필터링 하여 나타냄
+  const getMyReservation = async () => {
+    try {
+      //const response = await getMyReservationList(user_id);
+      const response = await getMyReservationList('piona');
+      const filteredByStatus = response.data.filter(
+        item => item.status !== 'R',
+      );
+      setData(filteredByStatus);
+      console.log(filteredByStatus);
+    } catch (error) {
+      console.log('예약현황 조회 실패', error);
+    }
   };
 
-  const DATA = [
-    //괄호 하나하나가 item이 된다.
-    {
-      shopName: '호진이가게',
-      date: '2020-20-20',
-      desc: '호진이가게,ㅁㄴㅇ',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '소원이가게',
-      date: '2020-20-20',
-      desc: '소원이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '혜성이가게',
-      date: '2020-20-20',
-      desc: '혜성이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '동준이형가게',
-      date: '2020-20-20',
-      desc: '동준이형가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '정아누나가게',
-      date: '2020-20-20',
-      desc: '정아누나가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '윤택이형가게',
-      date: '2020-20-20',
-      desc: '윤택이형가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '호진이가게',
-      date: '2020-20-20',
-      desc: '호진이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '호진이가게',
-      date: '2020-20-20',
-      desc: '호진이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '호진이가게',
-      date: '2020-20-20',
-      desc: '호진이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-    {
-      shopName: '호진이가게',
-      date: '2020-20-20',
-      desc: '호진이가게',
-      imgUrl: 'https://reactjs.org/logo-og.png',
-    },
-  ];
-
-  useEffect(() => {
-    getData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      getMyReservation();
+    }, []),
+  );
 
   const renderItem = ({item}) => {
-    return (
-      <DoneCard
-        //item과 func를 prop으로 전달
-        item={item}
-        func={() => navigation.navigate('RegisterReview')}
-      />
-    );
+    return <DoneCard item={item} navigation={navigation} />;
   };
 
   return (
@@ -115,15 +57,11 @@ const PicnicedPage = ({navigation}) => {
       <View>
         <FlatList
           //리스트의 소스를 담는 속성
-          //data={data}
-          data={DATA}
+          data={data}
           //data로 받은 소스의 아이템들을 render 시켜주는 콜백함수
           renderItem={renderItem}
           //item의 고유의 키를 부여하는 속성
-          keyExtractor={item => String(item.id)}
-          //무한 스크롤때문에 넣은듯
-          // onEndReached={() => {if(loading===false && pageNum<=totalPageCnt) getMyPillHistoryList()}}
-          // onEndReachedThreshold={0.4}
+          keyExtractor={item => item.reservation_id}
         />
       </View>
     </View>
