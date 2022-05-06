57d5af5ca781ee1aec8e6bdc53713b43b004f0a3
hanhs4544@naver.com <Gptjd1220!>
2022-05-06 12:05:59 +0900

    🔧 Add getReviewList API

--- web/backend/bloom/src/main/java/com/jeans/bloom/api/service/ReviewService.java
+++ web/backend/bloom/src/main/java/com/jeans/bloom/api/service/ReviewService.java
@@ -2,9 +2,12 @@ package com.jeans.bloom.api.service;
 
 
 import com.jeans.bloom.api.request.ReviewCommentReq;
+import com.jeans.bloom.api.response.ReviewRes;
 import com.jeans.bloom.db.entity.Review;
 import com.jeans.bloom.db.entity.ReviewComment;
 import com.jeans.bloom.db.entity.type.StatusType;
+
+import java.util.List;
 
 public interface ReviewService {
     Review findReviewByReviewId(Integer review_id) throws Exception;
@@ -14,4 +17,6 @@ public interface ReviewService {
     Review reviewReport(int review_id, StatusType type) throws Exception;
 
     Boolean deleteReview(int review_id) throws Exception;
+
+    List<ReviewRes> findReviewsByReservation_Shop_ShopNumber(String shopNumber) throws Exception;
 }
