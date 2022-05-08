ae0a08d232528a175698c8788d6d8cfe4c81fc3c
wjddk7507 <wjddk9494@gmail.com>
2022-05-08 20:42:58 +0900

    🔧 Add change reservation status API

--- web/backend/bloom/src/main/java/com/jeans/bloom/api/controller/ReservationController.java
+++ web/backend/bloom/src/main/java/com/jeans/bloom/api/controller/ReservationController.java
@@ -1,9 +1,12 @@
 package com.jeans.bloom.api.controller;
 
+import com.jeans.bloom.api.request.ReservationOrderStatusReq;
 import com.jeans.bloom.api.request.ReservationStatusReq;
 import com.jeans.bloom.api.response.ReservationRes;
 import com.jeans.bloom.api.service.ReservationService;
 import com.jeans.bloom.common.response.BaseResponseBody;
+import com.jeans.bloom.db.entity.type.OrderStatus;
+import com.querydsl.core.types.Order;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -28,13 +31,13 @@ public class ReservationController {
 
     /**
      * LJA | 2022.05.08
-     * @name getReview
+     * @name findReservationsByShop_ShopNumber
      * @api {get} /reservation?shop_number={shop_number}
      * @des 가게 사업자번호를 통해 전체 예약현황을 조회하는 메소드
      */
     @GetMapping
     @ApiOperation(value = "예약자 현황", notes = "가게 사업자번호를 입력받아 예약자 현황을 조회하는 메소드")
-    public ResponseEntity<BaseResponseBody> getReview(
+    public ResponseEntity<BaseResponseBody> findReservationsByShop_ShopNumber(
             @RequestParam @ApiParam(value = "가게 사업자번호", required = true) String shop_number
     ) {
         try {
@@ -47,14 +50,19 @@ public class ReservationController {
 
     /**
      * LJA | 2022.05.08
-     * @name ReservationController
-     * @api {get} /reservation/shop_number={shop_number}
-     * @des 기업페이지에서 예약관리 API 사용을 위한 Controller
+     * @name changeOrderStatus
+     * @api {patch} /reservation
+     * @des 예약의 OrderStatus를 변경해주는 메소드
      */
     @PatchMapping
-    @ApiOperation(value = "예약자 현황", notes = "가게 사업자번호를 입력받아 예약자 현황을 조회하는 메소드")
+    @ApiOperation(value = "예약 상태 변경", notes = "예약 번호와 변경하고자 하는 주문 상태를 입력받아 예약 상태를 변경해주는 메소드")
     public ResponseEntity<BaseResponseBody> changeOrderStatus(
-            @RequestBody @ApiParam(value = "", required = true)ReservationStatusReq reservationStatusReq) {
-        return null;
+            @RequestBody @ApiParam(value = "예약번호와 상태", required = true) ReservationOrderStatusReq reservationOrderStatusReq) {
+        try {
+            reservationService.changeOrderStatus(reservationOrderStatusReq.getReservation_id(), reservationOrderStatusReq.getStatus());
+            return ResponseEntity.status(200).body(BaseResponseBody.of("success"));
+        } catch(Exception e) {
+            return ResponseEntity.status(403).body(BaseResponseBody.of("fail", e));
+        }
     }
 }
