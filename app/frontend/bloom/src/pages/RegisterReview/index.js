7e834345801f39ba0af05689128b6a507c19d162
이호진 <roycmlhj@gmail.com>
2022-05-10 14:40:30 +0900

    🔧 Add registerReview API (only one picture)

--- app/frontend/bloom/src/pages/RegisterReview/index.js
+++ app/frontend/bloom/src/pages/RegisterReview/index.js
@@ -14,35 +14,31 @@ import {
   Button,
   FlatList,
   SafeAreaView,
+  ScrollView,
 } from 'react-native';
 import Stars from 'react-native-stars';
 import {useSelector} from 'react-redux';
 import {RegisterReviewApi} from '../../utils/Axios';
-// //import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-// import MultipleImagePicker from '@baronha/react-native-multiple-image-picker';
 import ImagePicker from 'react-native-image-crop-picker';
-
+import ImageResizer from 'react-native-image-resizer';
 /**
- * LHJ | 2022.05.09
+ * LHJ | 2022.05.10
  * @name RegisterReview
- * @api x
+ * @api RegisterReviewApi
  * @des
  * 1. 컴포넌트 목록 :
  * 2. 페이지 기능 :
- * 리뷰 등록 모달을 띄울 페이지이다.
+ * 리뷰 등록 페이지이다.
+ * 별점, 내용, 사진등을 바탕으로 api를 통해서 리뷰를 등록한다
+ * 현재(05.10기준)는 사진이 한장만 등록된다.
+ * 3. api : RegisterReviewApi (리뷰 등록 api)
  */
 
-//navigation으로 넘겨줄때 값은 route로 가져온다.
-//route.params.reservation_id
 const RegisterReview = ({navigation, route}) => {
-  //route.params.reservationId
-  //route.params.user_id
-  //route.params.shop_name
   const token = useSelector(state => state.user.accessToken);
   const user_id = useSelector(state => state.user.id);
   const [comment, onChangeText] = React.useState(null);
   const [starCount, setState] = useState(1);
-  //const [file, setImage] = useState('');
 
   const [kwClean, setKwClean] = useState('N');
   const [kwReasonable, setKwReasonable] = useState('N');
@@ -57,30 +53,6 @@ const RegisterReview = ({navigation, route}) => {
   const [kwKindColor, setKwKindColor] = useState('#C0C0C0');
   const [kwAdorableColor, setKwAdorableColor] = useState('#C0C0C0');
   const [kwMoodColor, setKwMoodColor] = useState('#C0C0C0');
-
-  let formData = new FormData();
-
-  const register = async () => {
-    formData.append('score', starCount);
-    formData.append('kwClean', kwClean);
-    formData.append('kwReasonable', kwReasonable);
-    formData.append('kwVarious', kwVarious);
-    formData.append('file', image);
-    formData.append('reservationId', route.params.reservationId);
-    formData.append('kwKind', kwKind);
-    formData.append('kwAdorable', kwAdorable);
-    formData.append('kwMood', kwMood);
-    formData.append('userid', user_id);
-    formData.append('content', comment);
-    console.log(formData);
-    try {
-      const response = await RegisterReviewApi(formData, token);
-      //console.log(response.data);
-      //console.log(JSON.stringify(formData));
-    } catch (error) {
-      console.log('리뷰 등록 실패', error);
-    }
-  };
 
   const setResonble = () => {
     if (kwReasonable === 'N') {
@@ -142,101 +114,130 @@ const RegisterReview = ({navigation, route}) => {
     }
   };
 
-  //const state = {avatar: ''};
-  // const [image, setImagesss] = useState('');
-
-  // const showImage = () => {
-  //   launchImageLibrary({}, response => {
-  //     //alert(response.uri);
-  //     setImagesss(response.assets[0].uri);
-  //     console.warn(response.assets[0].uri);
-  //   });
-  // };
-
-  const [images, setImages] = useState([]);
-  // const [preview, setPreview] = useState();
-  // setPreview({uri: `data:${response.mime};base64,${response.data}`});
   const [image, setImage] = useState({uri: '', name: '', type: ''});
-  let list = [];
-  const openImagePicker = () => {
-    ImagePicker.openPicker({
-      //includeBase64: true,
-      multiple: true,
-      waitAnimationEnd: false,
+  const [preview, setPreview] = useState([{uri: ''}]);
+
+  const onChangeFile = useCallback(() => {
+    return ImagePicker.openPicker({
       includeExif: true,
-      forceJpg: true,
-      maxFiles: 5,
-      compressImageQuality: 0.8,
+      includeBase64: true,
       mediaType: 'photo',
+      //multiple: true,
+      // maxFiles: 5,
+      // compressImageQuality: 0.8,
     })
-      .then(image => {
-        image.map((item, index) => {
-          setImage({uri: item.path, type: 'image/jpeg', name: item.path});
-          list.push(image);
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
 
-          console.log('list' + list);
-          // formData.append(
-          //   'file',
-          //   //{
-          //     // uri: item.path,
-          //     // type: 'image/jpeg',
-          //     // name: item.path,
-          //     //name: item.filename || `temp_image_${index}.jpg`,
-          //     //}
-          //     image,
-          //     );
-              setImages(image);
-              console.log(image);
-        });
-      })
-      .catch(e => alert(e));
-  };
+  const onResponse = useCallback(async response => {
+    console.log(response.width, response.height, response.exif);
+    setPreview({uri: `data:${response.mime};base64,${response.data}`});
+    //response.map((item, index) => {
+    //   setPreview({uri: `data:${item.mime};base64,${item.data}`});
+    // });
+    // console.log(preview);
+    return ImageResizer.createResizedImage(
+      response.path,
+      600,
+      600,
+      response.mime.includes('jpeg') ? 'JPEG' : 'PNG',
+      100,
+      0,
+    ).then(r => {
+      console.log(r.uri, r.name);
+      setImage({
+        uri: r.uri,
+        name: r.name,
+        type: response.mime,
+      });
+    });
+  }, []);
 
-  const onDelete = value => {
-    const data = images.filter(
-      item =>
-        item?.localIdentifier &&
-        item?.localIdentifier !== value?.localIdentifier,
-    );
-    setImages(data);
-  };
-  const renderItem = ({item, index}) => {
-    return (
-      <View>
-        <Image
-          width={IMAGE_WIDTH}
-          source={{
-            uri:
-              item?.type === 'video'
-                ? item?.thumbnail ?? ''
-                : 'file://' + (item?.crop?.cropPath ?? item.path),
-          }}
-          style={styles.media}
-        />
-        {/* <TouchableOpacity
-          onPress={() => onDelete(item)}
-          activeOpacity={0.9}
-          style={styles.buttonDelete}>
-          <Text style={styles.titleDelete}>삭제</Text>
-        </TouchableOpacity> */}
-      </View>
-    );
-  };
+  const onComplete = useCallback(async () => {
+    if (!image) {
+      Alert.alert('알림', '파일을 업로드해주세요.');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('score', starCount);
+    formData.append('kwClean', kwClean);
+    formData.append('kwReasonable', kwReasonable);
+    formData.append('kwVarious', kwVarious);
+    formData.append('file', image);
+    formData.append('reservationId', route.params.reservationId);
+    formData.append('kwKind', kwKind);
+    formData.append('kwAdorable', kwAdorable);
+    formData.append('kwMood', kwMood);
+    formData.append('userid', user_id);
+    formData.append('content', comment);
+    formData.append('image', image);
+    try {
+      await RegisterReviewApi(formData, token);
+      console.log('알림', '완료처리 되었습니다.');
+      navigation.goBack();
+    } catch (error) {
+      Alert.alert('sss');
+    }
+  }, [
+    comment,
+    image,
+    kwAdorable,
+    kwClean,
+    kwKind,
+    kwMood,
+    kwReasonable,
+    kwVarious,
+    navigation,
+    route.params.reservationId,
+    starCount,
+    token,
+    user_id,
+  ]);
+
+  // const renderItem = ({item, index}) => {
+  //   return (
+  //     <View>
+  //       <Image
+  //         width={IMAGE_WIDTH}
+  //         source={{
+  //           uri:
+  //             // item?.type === 'video'
+  //             //   ? item?.thumbnail ?? ''
+  //             //   : 'file://' + (item?.crop?.cropPath ?? item.path),
+  //             item.uri,
+  //         }}
+  //         style={styles.media}
+  //       />
+  //       {/* <TouchableOpacity
+  //         onPress={() => onDelete(item)}
+  //         activeOpacity={0.9}
+  //         style={styles.buttonDelete}>
+  //         <Text style={styles.titleDelete}>삭제</Text>
+  //       </TouchableOpacity> */}
+  //     </View>
+  //   );
+  // };
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
+      <ScrollView>
+      {/* <FlatList
         style={[
           styles.container,
           {
             paddingTop: 6,
           },
         ]}
-        data={images}
-        keyExtractor={(item, index) => (item?.filename ?? item?.path) + index}
+        data={preview}
+        keyExtractor={(item, index) => item.uri + index}
         renderItem={renderItem}
         numColumns={3}
-      />
+      /> */}
+      {/* 기존의 플랫리스트 부분은 이미지 슬라이더 적용해서 구현하면 될 듯하다 */}
+      <View style={styles.preview}>
+        {preview && <Image style={styles.previewImage} source={preview} />}
+      </View>
       <View>
         <Text>{route.params.shop_name}의 서비스에 만족하셨나요?</Text>
       </View>
@@ -272,44 +273,44 @@ const RegisterReview = ({navigation, route}) => {
           </View>
         </TouchableOpacity>
         <TouchableOpacity
+        style={{backgroundColor: kwMoodColor}}
           onPress={() => {
             setMood();
           }}>
           <Text>감성 넘쳐요</Text>
         </TouchableOpacity>
         <TouchableOpacity
+        style={{backgroundColor: kwCleanColor}}
           onPress={() => {
             setClean();
           }}>
           <Text>깔끔해요</Text>
         </TouchableOpacity>
         <TouchableOpacity
+        style={{backgroundColor: kwAdorableColor}}
           onPress={() => {
             setAdorable();
           }}>
           <Text>아기자기해요</Text>
         </TouchableOpacity>
         <TouchableOpacity
+        style={{backgroundColor: kwVariousColor}}
           onPress={() => {
             setVarious();
           }}>
           <Text>구성이 다양해요</Text>
         </TouchableOpacity>
         <TouchableOpacity
+        style={{backgroundColor: kwKindColor}}
           onPress={() => {
             setKind();
           }}>
           <Text>친절해요</Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            openImagePicker();
-          }}>
-          <Text>select images</Text>
-        </TouchableOpacity>
-      </View>
+      <Pressable style={styles.button} onPress={onChangeFile}>
+        <Text style={styles.buttonText}>이미지 선택</Text>
+      </Pressable>
       <View style={styles.menuAddBtn}>
         <TouchableOpacity
           style={{
@@ -322,14 +323,14 @@ const RegisterReview = ({navigation, route}) => {
             justifyContent: 'center',
           }}
           onPress={() => {
-            '#';
-            register();
+            onComplete();
           }}>
           <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
             완료
           </Text>
         </TouchableOpacity>
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -385,6 +386,26 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 12,
     color: '#000',
+  },
+  preview: {
+    marginHorizontal: 10,
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 3,
+    backgroundColor: '#D2D2D2',
+    marginBottom: 10,
+  },
+  previewImage: {
+    height: Dimensions.get('window').height / 3,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: 'blue',
+    marginBottom: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
 
