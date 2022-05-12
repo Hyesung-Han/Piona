655aa764881109fd30bbccd93c44ba33e23a8814
wjddk7507 <wjddk9494@gmail.com>
2022-05-12 19:24:24 +0900

    🔧 Update update shop info search address

--- web/frontend/bloom/src/sections/@dashboard/user/account/AccountGeneral.js
+++ web/frontend/bloom/src/sections/@dashboard/user/account/AccountGeneral.js
@@ -4,9 +4,13 @@ import { useCallback, useEffect, useState } from 'react';
 // form
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+// swal
+import Swal from 'sweetalert2';
 // @mui
-import { Box, Grid, Card, Stack, Typography, Button } from '@mui/material';
+import { Box, Grid, Card, Stack, Typography, Button, Dialog, TextField, DialogActions, DialogContent } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+// daum postcode api
+import DaumPostCode from 'react-daum-postcode';
 // hooks
 import useAuth from '../../../../hooks/useAuth';
 // utils
@@ -18,10 +22,12 @@ import { FormProvider, RHFTextField, RHFUploadAvatar } from '../../../../compone
 // ----------------------------------------------------------------------
 
 export default function AccountGeneral({shop}) {
-  const { enqueueSnackbar } = useSnackbar();
   // const { user_id, shop_number, access_token } = JSON.parse(localStorage.getItem("user"));
   const { user } = useAuth();
-  const { currentImageUrl } = shop?.image_url || '';
+  const [currentImageUrl, setCurrentImageUrl] = useState(shop?.image_url || '');
+  const [isOpen, setIsOpen] = useState(false);
+  const [zip_code, setZipCode] = useState();
+  const [address, setAddress] = useState();
 
   const UpdateUserSchema = Yup.object().shape({
     name: Yup.string().required('Name is required'),
@@ -30,8 +36,6 @@ export default function AccountGeneral({shop}) {
   const defaultValues = {
     shop_number: shop?.shop_number || '',
     name: shop?.name || '',
-    zip_code: shop?.zip_code || '',
-    address: shop?.address || '',
     detail_address: shop?.detail_address || '',
     hours: shop?.hours || '',
     tel: shop?.tel || '',
@@ -51,18 +55,23 @@ export default function AccountGeneral({shop}) {
     formState: { isSubmitting },
   } = methods;
 
+  useEffect(()=>{
+    console.log("zip_code",zip_code);
+    console.log("address",address);
+  },[zip_code, address]);
+
   const onSubmit = async (shopInfo) => {
-      const { shop_number, address, description, detail_address, hours, image_url, name, tel, url, zip_code } = shopInfo;
+      const { shop_number, description, detail_address, hours, image_url, name, tel, url } = shopInfo;
       const shop_lng = 0;
       const shop_lat = 0;
-
+      
       const fd = new FormData();
-      if(typeof image_url === 'string') {
-        // fd.append('file', null);
+      if(typeof image_url === 'string' || image_url === '') {
+        setCurrentImageUrl('');
       } else {
-        console.log("파일있")
         fd.append('file', image_url);
       }
+
       fd.append('shopInfoReq.address', address);
       fd.append('shopInfoReq.description', description);
       fd.append('shopInfoReq.detail_address', detail_address);
@@ -81,13 +90,12 @@ export default function AccountGeneral({shop}) {
             Authorization: user.access_token
         }});
         const { data } = response;
-        console.log(data);
+        if(data.result === 'success') {
+          Swal.fire('수정 되었습니다.')
+        }
       } catch (e) {
         console.error(e);
       }
-      
-      // await new Promise((resolve) => setTimeout(resolve, 500));
-      // enqueueSnackbar('Update success!');
     };
 
   const handleDrop = useCallback(
@@ -105,13 +113,34 @@ export default function AccountGeneral({shop}) {
     },
     [setValue]
   );
-  
-  const onClickSearchAddress = () => {
-    console.log("주소검색");
-    console.log("하위shop", shop);
-  };
 
-  // if (shop.length === 0 || !shop) return <>loading중..</>;
+  const openPostCode = () => {
+    setIsOpen(true);
+  }
+
+  const closePostCode = () => {
+    setIsOpen(false);
+  }
+
+  const handlePostCode = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+        if (data.bname !== '') {
+            extraAddress += data.bname;
+        }
+        if (data.buildingName !== '') {
+            extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+        }
+        fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        
+        setZipCode(data.zonecode);
+        setAddress(fullAddress);
+    }
+    closePostCode();
+  }
+
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Grid container spacing={3}>
@@ -158,18 +187,17 @@ export default function AccountGeneral({shop}) {
                   gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: '2fr 1fr' },
                 }}
               >
-                <RHFTextField name="zip_code" label="우편번호" />
+                <TextField name="zip_code" value={zip_code} placeholder="우편번호" disabled onClick={openPostCode} />
                 <Button
                   sx={{ml:3}}
-                  onClick={()=>onClickSearchAddress()}>주소검색</Button>
+                  onClick={()=>openPostCode()}>주소검색</Button>
               </Box>
-              <RHFTextField name="address" label="주소" />
+              <TextField name="address" value={address} placeholder="주소" disabled onClick={openPostCode} />
               <RHFTextField name="detail_address" label="상세주소" />
               <RHFTextField name="hours" multiline rows={2} label="영업시간" />
               <RHFTextField name="tel" label="가게 전화번호" />
               <RHFTextField name="url" label="가게 사이트 주소(SNS, 웹사이트 등)" />
             </Box>
-
             <Stack spacing={3} alignItems="flex-end" sx={{ mt: 2 }}>
               <RHFTextField name="description" multiline rows={4} label="가게 설명" />
 
@@ -180,6 +208,17 @@ export default function AccountGeneral({shop}) {
           </Card>
         </Grid>
       </Grid>
+      <Dialog
+        open={isOpen}
+        onClose={closePostCode}
+      >
+        <DialogContent>
+          <DaumPostCode onComplete={handlePostCode} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closePostCode}>닫기</Button>
+        </DialogActions>
+      </Dialog>
     </FormProvider>
   );
 }
