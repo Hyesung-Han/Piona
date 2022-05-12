c90320fcbc0e9e0c179e13cae5c8bf9da42383c1
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-05-12 11:58:35 +0900

    🎨 Change ShopHome CSS

--- app/frontend/bloom/src/pages/ShopHome/index.js
+++ app/frontend/bloom/src/pages/ShopHome/index.js
@@ -1,6 +1,14 @@
 import React, {useState, useCallback} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
-import {View, Dimensions, Alert, Text, Image, ScrollView} from 'react-native';
+import {
+  View,
+  Dimensions,
+  Alert,
+  Text,
+  Image,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import {useSelector} from 'react-redux';
 import {shopDetailAPI} from '../../utils/Axios';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -13,7 +21,7 @@ import NaverMapView, {
 } from 'react-native-nmap';
 
 /**
- * LHJ | 2022.05.09
+ * LHJ, CSW | 2022.05.12
  * @name shopHome
  * @api shopDetailAPI/getShopDetail
  * @des
@@ -21,22 +29,7 @@ import NaverMapView, {
  * 2. 해당 페이지 설명 : 리덕스에 저장된 shopNumber를 뽑아와서 api(shopDetailAPI/getShopDetail)를 호출한다.
  * 해당 페이지(shopHome)에서는 가게 상세 정보 api를 호출한다.
  */
-//  data: {
-//   address: "서울특별시 가나다라동"
-//   description: "우리가게가제일조아용"
-//   hours: "아홉시부터열시까지바짝벌어요"
-//   image_url: "bdkdfpdf"
-//   name: "pipipi"
-//   review_cnt: 4
-//   score: 2.25
-//   shop_lat: 37.359357
-//   shop_lng: 127.104835
-//   shop_number: "10"
-//   tel: "031121513"
-//   url: "dbdkfdpf"
-//   wish_id: 0
-//   zip_code: 21364
-//  }
+
 const ShopHomePage = ({navigation, route}) => {
   const [data, setData] = useState([]);
   // const [modalVisible, setModalVisible] = useState(true);
@@ -52,7 +45,6 @@ const ShopHomePage = ({navigation, route}) => {
     latitude: 0.0,
     longitude: 0.0,
   });
-
   const getShopDetailInfo = async () => {
     try {
       const res = await shopDetailAPI.getShopDetail(shopNumber, token);
@@ -72,6 +64,22 @@ const ShopHomePage = ({navigation, route}) => {
     }
   };
 
+  const startScore = () => {
+    const result = [];
+    const row = Math.floor(data.score);
+    for (let i = 0; i < row; i++) {
+      result.push(
+        <Icon
+          name="star"
+          color="#F2A344"
+          backgroundColor="transparent"
+          size={15}
+        />,
+      );
+    }
+    return result;
+  };
+
   useFocusEffect(
     useCallback(() => {
       getShopDetailInfo();
@@ -79,51 +87,142 @@ const ShopHomePage = ({navigation, route}) => {
   );
 
   return (
-    <ScrollView>
-      <View>
-        <Text>{data.name}</Text>
-        <Icon
-          name="star"
-          color="#F2A344"
-          backgroundColor="transparent"
-          size={15}
-        />
-        <Text>{data.score}/5</Text>
-        <Text>방문자리뷰 {data.review_cnt}</Text>
-        <Icon
-          name="compass-outline"
-          color="#F2A344"
-          backgroundColor="transparent"
-          size={15}
-        />
-        <Text>{data.address}</Text>
-        <Text>{data.hours}</Text>
-        <Text>{data.tel}</Text>
-        <Text>{data.url}s</Text>
-        <Image
-          source={{uri: `${data.image_url}`}}
-          style={{
-            resizeMode: 'contain',
-            width: '100%',
-            height: 130,
-            borderRadius: 10,
-          }}
-        />
-        <View
-          style={{
-            width: Dimensions.get('window').width,
-            height: 200,
-            marginTop: 10,
-          }}>
-          <NaverMapView
-            style={{width: '100%', height: '80%'}}
-            zoomControl={false}
-            center={center}>
-            <Marker coordinate={coordinate} pinColor="blue" />
-          </NaverMapView>
+    <ScrollView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.imgBox}>
+          <Image
+            source={{uri: `${data.image_url}`}}
+            style={{
+              resizeMode: 'contain',
+              width: '90%',
+              height: '100%',
+              borderRadius: 10,
+            }}
+          />
+        </View>
+        <View style={styles.nameBox}>
+          <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
+            {data.name}
+          </Text>
+          <View style={styles.reviewBox}>
+            <View style={styles.starIcons}>{startScore()}</View>
+
+            <Text style={{marginHorizontal: 5}}>{data.score}/5</Text>
+            <Text style={{marginHorizontal: 5}}>
+              방문자리뷰 {data.review_cnt}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.infoBox}>
+          <View
+            style={{
+              width: '100%',
+              height: 160,
+            }}>
+            <NaverMapView
+              style={{width: '100%', height: '100%'}}
+              zoomControl={false}
+              center={center}>
+              <Marker coordinate={coordinate} pinColor="blue" />
+            </NaverMapView>
+          </View>
+          <View style={styles.descriptionBox}>
+            <View style={styles.icons}>
+              <Icon
+                name="location-sharp"
+                color="#A3A3A3"
+                backgroundColor="transparent"
+                size={20}
+              />
+              <Text style={{color: 'black', fontSize: 13, marginLeft: 10}}>
+                {data.address}
+              </Text>
+            </View>
+            <View style={styles.icons}>
+              <Icon
+                name="time-outline"
+                color="#A3A3A3"
+                backgroundColor="transparent"
+                size={20}
+              />
+              <Text style={{color: 'black', fontSize: 13, marginLeft: 10}}>
+                {data.hours}
+              </Text>
+            </View>
+            <View style={styles.icons}>
+              <Icon
+                name="call-outline"
+                color="#A3A3A3"
+                backgroundColor="transparent"
+                size={20}
+              />
+              <Text style={{color: 'black', fontSize: 13, marginLeft: 10}}>
+                {data.tel}
+              </Text>
+            </View>
+            <View style={styles.icons}>
+              <Text style={{color: 'black', fontSize: 13, marginLeft: 35}}>
+                {data.url}
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgBox: {
+    width: '100%',
+    height: 180,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nameBox: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  infoBox: {
+    width: '90%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  descriptionBox: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    width: '100%',
+    backgroundColor: '#F8F8F8',
+    paddingVertical: 20,
+  },
+  reviewBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 5,
+    alignItems: 'center',
+  },
+  starIcons: {
+    flexDirection: 'row',
+    marginLeft: 10,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 4,
+    paddingHorizontal: 8,
+  },
+});
+
 export default ShopHomePage;
