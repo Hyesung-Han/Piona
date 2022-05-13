1db1e53f1195d6f8ebaaa85f3e77f84cc6c296c3
wjddk7507 <wjddk9494@gmail.com>
2022-05-13 11:27:20 +0900

    🎨 Change ShopProductCard

--- web/frontend/bloom/src/sections/@dashboard/e-commerce/shop/ShopProductCard.js
+++ web/frontend/bloom/src/sections/@dashboard/e-commerce/shop/ShopProductCard.js
@@ -3,7 +3,7 @@ import { paramCase } from 'change-case';
 import { Link as RouterLink } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 // @mui
-import { Box, Card, Link, Typography, Stack } from '@mui/material';
+import { Box, Card, Link, Typography, Stack, CardActionArea } from '@mui/material';
 // routes
 import { PATH_DASHBOARD } from '../../../../routes/paths';
 // utils
@@ -29,50 +29,43 @@ export default function ShopProductCard({ product }) {
   }, []);
 
   return (
-    <Card>
-      <Box sx={{ position: 'relative' }}>
-        {/* {status && (
-          <Label
-            variant="filled"
-            color={(status === 'sale' && 'error') || 'info'}
-            sx={{
-              top: 16,
-              right: 16,
-              zIndex: 9,
-              position: 'absolute',
-              textTransform: 'uppercase',
-            }}
-          >
-            {status}
-          </Label>
-        )} */}
-
-        <Image alt={name} src={image_url} ratio="1/1" />
-      </Box>
-
-      <Stack spacing={2} sx={{ p: 3 }}>
-        {/* <Link to='/dashboard/e-commerce/product/nike-air-force-1-ndestrukt' color="inherit" component={RouterLink}> */}
-        <Link to={`/dashboard/e-commerce/product/${item_id}`} color="inherit" component={RouterLink}>
-          <Typography variant="subtitle1" noWrap>
-            {name}
-          </Typography>
-        </Link>
-
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
-          {/* <ColorPreview colors={colors} /> */}
-
-          <Stack direction="row" >
-            {/* {priceSale && (
-              <Typography component="span" sx={{ color: 'text.disabled', textDecoration: 'line-through' }}>
-                {fCurrency(priceSale)}
-              </Typography>
+    <Link to={`/dashboard/e-commerce/product/${item_id}`} color="inherit" component={RouterLink}>
+      <Card>
+        <CardActionArea>
+          <Box sx={{ position: 'relative' }}>
+            {/* {status && (
+              <Label
+                variant="filled"
+                color={(status === 'sale' && 'error') || 'info'}
+                sx={{
+                  top: 16,
+                  right: 16,
+                  zIndex: 9,
+                  position: 'absolute',
+                  textTransform: 'uppercase',
+                }}
+              >
+                {status}
+              </Label>
             )} */}
 
-            <Typography variant="subtitle1">{price}원</Typography>
-            <Typography variant="subtitle2" color="rgba(132, 132, 132, 1)" ml={11}>수량 : {total_quantity}개</Typography>
+            <Image alt={name} src={image_url} ratio="1/1" />
+          </Box>
+
+          <Stack spacing={2} sx={{ p: 3 }}>
+              <Typography variant="subtitle1" noWrap>
+                {name}
+              </Typography>
+
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+              <Stack direction="row" sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }} >
+                <Typography variant="subtitle1">{price}원</Typography>
+                <Typography variant="subtitle2" color="rgba(132, 132, 132, 1)" ml={11}>수량 : {total_quantity}개</Typography>
+              </Stack>
+            </Stack>
           </Stack>
-        </Stack>
-      </Stack>
-    </Card>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
