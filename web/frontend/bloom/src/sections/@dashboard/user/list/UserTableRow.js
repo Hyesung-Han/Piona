c83d45817930b071be1e85f0f4ae73209f49ec86
wjddk7507 <wjddk9494@gmail.com>
2022-05-13 15:47:37 +0900

    🔧 Add Admin Page and function

--- web/frontend/bloom/src/sections/@dashboard/user/list/UserTableRow.js
+++ web/frontend/bloom/src/sections/@dashboard/user/list/UserTableRow.js
@@ -2,28 +2,32 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Avatar, Checkbox, TableRow, TableCell, Typography, MenuItem } from '@mui/material';
+import { TableRow, TableCell, MenuItem } from '@mui/material';
+// swal
+import Swal from 'sweetalert2';
 // components
 import Label from '../../../../components/Label';
 import Iconify from '../../../../components/Iconify';
 import { TableMoreMenu } from '../../../../components/table';
+// utils
+import axios from '../../../../utils/axios';
 
 // ----------------------------------------------------------------------
 
 UserTableRow.propTypes = {
   row: PropTypes.object,
   selected: PropTypes.bool,
-  onEditRow: PropTypes.func,
-  onSelectRow: PropTypes.func,
-  onDeleteRow: PropTypes.func,
 };
 
-export default function UserTableRow({ row, selected, onEditRow, onSelectRow, onDeleteRow }) {
+export default function UserTableRow({ row, selected, getUserList }) {
   const theme = useTheme();
 
-  const { name, avatarUrl, company, role, isVerified, status } = row;
+  const { user_id, nickname, phone, is_del, created_at } = row;
 
   const [openMenu, setOpenMenuActions] = useState(null);
+
+  const user = localStorage.getItem('user');
+  const parseUser = JSON.parse(user);
 
   const handleOpenMenu = (event) => {
     setOpenMenuActions(event.currentTarget);
@@ -33,44 +37,45 @@ export default function UserTableRow({ row, selected, onEditRow, onSelectRow, on
     setOpenMenuActions(null);
   };
 
+  const onClickBanHandler = async() => {
+    try {
+      const response = await axios.patch(`/api/admin?user_id=${row.user_id}&status_type=B`, {}, {
+        headers : {
+          Authorization: parseUser.access_token,
+        }
+      });
+      const {data} = response;
+      if(data.result === 'success') {
+        getUserList();
+        Swal.fire('수정 되었습니다.')
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
   return (
     <TableRow hover selected={selected}>
-      <TableCell padding="checkbox">
-        <Checkbox checked={selected} onClick={onSelectRow} />
-      </TableCell>
 
-      <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar alt={name} src={avatarUrl} sx={{ mr: 2 }} />
-        <Typography variant="subtitle2" noWrap>
-          {name}
-        </Typography>
-      </TableCell>
+      <TableCell align="left">{user_id}</TableCell>
 
-      <TableCell align="left">{company}</TableCell>
+      <TableCell align="left">{nickname}</TableCell>
 
       <TableCell align="left" sx={{ textTransform: 'capitalize' }}>
-        {role}
+        {phone}
+      </TableCell>
+
+      <TableCell align="left" sx={{ textTransform: 'capitalize' }}>
+        {created_at.split('T')[0]}
       </TableCell>
 
       <TableCell align="center">
-        <Iconify
-          icon={isVerified ? 'eva:checkmark-circle-fill' : 'eva:clock-outline'}
-          sx={{
-            width: 20,
-            height: 20,
-            color: 'success.main',
-            ...(!isVerified && { color: 'warning.main' }),
-          }}
-        />
-      </TableCell>
-
-      <TableCell align="left">
         <Label
           variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
-          color={(status === 'banned' && 'error') || 'success'}
+          color={(is_del === 'B' && 'error') || 'success'}
           sx={{ textTransform: 'capitalize' }}
         >
-          {status}
+          {is_del==='N'?'Active':'Banned'}
         </Label>
       </TableCell>
 
@@ -83,22 +88,13 @@ export default function UserTableRow({ row, selected, onEditRow, onSelectRow, on
             <>
               <MenuItem
                 onClick={() => {
-                  onDeleteRow();
+                  onClickBanHandler();
                   handleCloseMenu();
                 }}
                 sx={{ color: 'error.main' }}
               >
                 <Iconify icon={'eva:trash-2-outline'} />
-                Delete
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  onEditRow();
-                  handleCloseMenu();
-                }}
-              >
-                <Iconify icon={'eva:edit-fill'} />
-                Edit
+                회원 정지
               </MenuItem>
             </>
           }
