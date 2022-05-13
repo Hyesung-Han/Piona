23ce533d6532aa441e1b07534b2e3f1253b07659
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-05-13 15:12:40 +0900

    🔨 Update CartCard CSS

--- app/frontend/bloom/src/components/CartCard/index.js
+++ app/frontend/bloom/src/components/CartCard/index.js
@@ -22,10 +22,8 @@ const CartCardList = props => {
   const image_url = props.item.image_url;
   const quantity = props.item.quantity;
   const total_quantity = props.item.total_quantity;
-  const reservation_date = props.item.reservation_date + '.960Z';
-  const res_date = props.item.reservation_date.split('T')[0];
+  const reservation_date = props.item.reservation_date.split('T')[0];
   const shop_name = props.item.shop_name;
-  const shop_number = props.item.shop_number;
 
   const selectCartItem = useCallback(() => {
     if (checkStatus) {
@@ -35,15 +33,8 @@ const CartCardList = props => {
           id: '',
           quantity: '',
           price: '',
-          shop_number: '',
-          item_id: '',
-          item_name: '',
-          image_url: '',
-          reservation_date: '',
-          shop_name: '',
         }),
       );
-      dispatch(cartSlice.actions.deleteSelectCart(cart_id));
       setcheckStaus(false);
     } else {
       // 담고, 분홍색으로
@@ -52,30 +43,11 @@ const CartCardList = props => {
           id: cart_id,
           quantity: quantity,
           price: price,
-          shop_number: shop_number,
-          item_id: item_id,
-          item_name: item_name,
-          image_url: image_url,
-          reservation_date: reservation_date,
-          shop_name: shop_name,
         }),
       );
-      dispatch(cartSlice.actions.selectCart(cart_id));
       setcheckStaus(true);
     }
-  }, [
-    checkStatus,
-    dispatch,
-    cart_id,
-    quantity,
-    price,
-    shop_number,
-    item_id,
-    item_name,
-    image_url,
-    reservation_date,
-    shop_name,
-  ]);
+  }, [checkStatus, cart_id, quantity, price, dispatch]);
 
   return (
     <View style={styles.container}>
@@ -112,17 +84,11 @@ const CartCardList = props => {
               />
             )}
           </View>
-          <View style={styles.informationBox}>
-            <View style={styles.TopBox}>
-              <View style={styles.shopname}>
-                <Text style={{fontSize: 15, color: 'black'}}>{shop_name}</Text>
-              </View>
-              <View style={styles.reservationDate}>
-                <Text style={{fontSize: 13, color: 'black'}}>{res_date}</Text>
-              </View>
-            </View>
-            <View style={styles.itemname}>
-              <Text style={{fontSize: 12}}>{item_name}</Text>
+        </View>
+        <View style={styles.informationBox}>
+          <View style={styles.TopBox}>
+            <View style={styles.shopname}>
+              <Text style={{fontSize: 15, color: 'black'}}>{shop_name}</Text>
             </View>
             <View style={styles.reservationDate}>
               <Text style={{fontSize: 13, color: 'black'}}>
