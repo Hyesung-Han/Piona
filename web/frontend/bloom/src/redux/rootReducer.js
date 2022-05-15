e72f873ef8b47c690ce4b514c1e5c00b0eec9883
wjddk7507 <wjddk9494@gmail.com>
2022-05-15 22:50:32 +0900

    🔨 Delete kanban, calendar, chat, mail page

--- web/frontend/bloom/src/redux/rootReducer.js
+++ web/frontend/bloom/src/redux/rootReducer.js
@@ -2,11 +2,7 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // slices
-import mailReducer from './slices/mail';
-import chatReducer from './slices/chat';
 import productReducer from './slices/product';
-import calendarReducer from './slices/calendar';
-import kanbanReducer from './slices/kanban';
 import authReducer from './slices/auth';
 
 // ----------------------------------------------------------------------
@@ -33,10 +29,6 @@ const AuthPersistConfig = {
 };
 
 const rootReducer = combineReducers({
-  mail: mailReducer,
-  chat: chatReducer,
-  calendar: calendarReducer,
-  kanban: kanbanReducer,
   product: persistReducer(productPersistConfig, productReducer),
   auth: persistReducer(AuthPersistConfig, authReducer),
 });
