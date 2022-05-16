a1c58f3a602a272484f2076567964ee46c7eb336
오윤택 <xoem00@gmail.com>
2022-05-16 16:03:14 +0900

    🔧 Add signup id check

--- web/frontend/bloom/src/layouts/dashboard/navbar/NavConfig.js
+++ web/frontend/bloom/src/layouts/dashboard/navbar/NavConfig.js
@@ -7,6 +7,7 @@ import SvgIconStyle from '../../../components/SvgIconStyle';
 
 // ----------------------------------------------------------------------
 
+
 const getIcon = (name) => <SvgIconStyle src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
 
 const ICONS = {
@@ -26,11 +27,11 @@ const ICONS = {
   menuItem: getIcon('ic_menu_item'),
 };
 
-const navConfig = [
-  // GENERAL
+const shopNavConfig = [
+  // shop
   // ----------------------------------------------------------------------
   {
-    subheader: 'general',
+    subheader: 'SHOP',
     items: [
       { 
         title: '상품 관리', 
@@ -46,9 +47,12 @@ const navConfig = [
       { title: '리뷰 관리', path: PATH_DASHBOARD.review.list, icon: ICONS.chat },
       { title: '정보 수정', path: PATH_DASHBOARD.user.account, icon: ICONS.user },
     ],
-  },
+  }
+];
 
-  // MANAGEMENT
+
+const adminNavConfig = [
+  // admin
   // ----------------------------------------------------------------------
   {
     subheader: 'admin',
@@ -57,7 +61,52 @@ const navConfig = [
       { title: '리뷰 관리', path: PATH_DASHBOARD.admin.review, icon: ICONS.chat},
       { title: '기업 관리', path: PATH_DASHBOARD.admin.shop, icon: ICONS.banking},
     ],
-  },
+  }
 ];
 
-export default navConfig;
+// const NavConfig = [];
+
+// const setNav = () => {
+//   const user = localStorage.getItem('user');
+//   if(user != null){
+//     const parseUser = JSON.parse(user);
+//     if(parseUser.user_code === 'A'){
+//       NavConfig.push(
+//         {
+//           subheader: 'admin',
+//           items: [
+//             { title: '회원 관리', path: PATH_DASHBOARD.admin.user, icon: ICONS.user,},
+//             { title: '리뷰 관리', path: PATH_DASHBOARD.admin.review, icon: ICONS.chat},
+//             { title: '기업 관리', path: PATH_DASHBOARD.admin.shop, icon: ICONS.banking},
+//           ],
+//         }
+//       )
+//       // return 
+      
+//     }
+//     NavConfig.push(
+//       {
+//         subheader: 'SHOP',
+//         items: [
+//           { 
+//             title: '상품 관리', 
+//             path: PATH_DASHBOARD.general.ecommerce, 
+//             icon: ICONS.cart,
+//             children: [
+//               { title: '상품 등록하기', path: PATH_DASHBOARD.items.new },
+//               { title: '상품 목록보기', path: PATH_DASHBOARD.items.list },
+//             ],
+//           },
+//           { title: '예약 관리', path: PATH_DASHBOARD.reservation.list, icon: ICONS.booking },
+//           { title: '정산 관리', path: PATH_DASHBOARD.sale.list, icon: ICONS.ecommerce },
+//           { title: '리뷰 관리', path: PATH_DASHBOARD.review.list, icon: ICONS.chat },
+//           { title: '정보 수정', path: PATH_DASHBOARD.user.account, icon: ICONS.user },
+//         ],
+//       },
+//     )
+//   }
+// };
+
+export {shopNavConfig, adminNavConfig} ;
+
+
